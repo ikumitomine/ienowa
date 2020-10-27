505295a46f20ec88a062651b3b758ebe3e67560a
vagrant <vagrant@localhost.localdomain>
2020-10-27 03:03:10 +0000

    [add]favorite

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -3,7 +3,12 @@ class BanksController < ApplicationController
   	@banks = Loan.joins(:bank).group(:bank_id).pluck("banks.id,banks.name,min(loans.rate)")
   end
 
-  def favorite
-  	# @coupon = Coupon.find(params[:id])
+  def show
+  	@bank = Bank.find(params[:id])
+  	#@loans = @bank.loan
+  end
+
+  def favorites
+  	@banks = current_user.fav_banks.includes(:user)
   end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,16 @@
 class FavoritesController < ApplicationController
+
+	def create
+		bank = Bank.find(params[:bank_id])
+		favorite = current_user.favorites.new(bank_id: params[:bank_id])
+		favorite.save!
+		redirect_to bank_path(bank)
+	end
+
+	def destroy
+		bank = Bank.find(params[:bank_id])
+		current_user.favorites.find_by(bank_id: params[:bank_id]).destroy!
+		redirect_to bank_path(bank)
+	end
+
 end
