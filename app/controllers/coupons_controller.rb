c3b384bc580b32c0cd1272b7ff9b6939f20db80d
vagrant <vagrant@localhost.localdomain>
2020-10-29 09:22:46 +0000

    [fix]ja_coupon

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,11 +3,11 @@ class CouponsController < ApplicationController
   end
 
   def index
-  	@coupons = Coupon.where(user_id: current_user.id, is_valid: 'ok')
+  	@coupons = Coupon.where(user_id: current_user.id, is_valid: true)
   end
 
   def show
-  	@coupon = Coupon.find(params[:id])
+  	@coupon = Coupon.find_by(params[:id], is_valid: true)
   end
 # 詳細ページでも有効のものを表示させる.where(is_valid: 'ok')
 end
