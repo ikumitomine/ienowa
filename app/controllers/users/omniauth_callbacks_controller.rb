b1e6fa0c4a02def7d1231ac2c09683abe3469e7d
vagrant <vagrant@localhost.localdomain>
2020-11-01 10:03:35 +0000

    [fix]auth_signin

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def google
 		@user = User.find_for_google(request.env['omniauth.auth'])
 
+		# 返ってくるデータがDBに保存されているか確認し、保存に失敗したらサインアップ画面へリダイレクトする
 		if @user.persisted?
 			flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
 			sign_in_and_redirect @user, event: :authentication
