c7d7b55acf6541a51d5b3f8903e5b986a397e6e9
vagrant <vagrant@localhost.localdomain>
2020-11-07 10:37:43 +0000

    [add]piechart_linechart

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -8,8 +8,125 @@ class BanksController < ApplicationController
   end
 
   def show
-      @bank = Bank.find(params[:id])
-      @loans = @bank.loans.page(params[:page]).order("borrowing_year DESC").order("borrowing_month DESC").per(5)
+    @bank = Bank.find(params[:id])
+    @loans = @bank.loans.page(params[:page]).order("borrowing_year DESC").order("borrowing_month DESC").per(5)
+    @min_rate = Loan.joins(:bank).group(:bank_id).order("rate ASC").pluck("min(loans.rate)") # 最安金利
+    #@average_rate = Loan.joins(:bank).search(@search_params).group(:bank_id).pluck("average(loans.rate)")
+
+    # チャート表記用のデータを整形
+    @loan = @bank.loans
+
+    @rate = @loan.pluck(:rate)
+    @aggregate_rate = aggregateRate(@rate)
+
+    @age = @loan.pluck(:age)
+    @aggregate_age = aggregateAge(@age)
+
+    @income = @loan.pluck(:income)
+    @aggregate_income = aggregateIncome(@income)
+
+    @family_form = @loan.pluck(:family_form)
+    @aggregate_family_form = aggregateFamilyForm(@family_form)
+
+    @job_period = @loan.pluck(:job_period)
+    @aggregate_job_period = aggregateJobPeriod(@job_period)
+
+  end
+
+  # 金利の棒グラフの集計範囲とラベル名を定義
+  def aggregateRate(array)
+    result = [["0.4%未満",0],["0.4%台",0],["0.5%台",0],["0.6%台",0],["0.7%台",0],["0.8%台",0],["0.9%台",0],["1%台",0],["2%以上",0]]
+    array.each do |i|
+      if i < 0.4
+        result[0][1] += 1
+      elsif 0.4 <= i && i < 0.5
+        result[1][1] += 1
+      elsif 0.5 <= i && i < 0.6
+        result[2][1] += 1
+      elsif 0.6 <= i && i < 0.7
+        result[3][1] += 1
+      elsif 0.7 <= i && i < 0.8
+        result[4][1] += 1
+      elsif 0.8 <= i && i < 0.9
+        result[5][1] += 1
+      elsif 0.9 <= i && i < 1
+        result[6][1] += 1
+      elsif 1 <= i && i < 2
+        result[7][1] += 1
+      else
+        result[8][1] += 1
+      end
+    end
+    return result
+  end
+
+  # 円グラフの集計の範囲とラベル名を定義
+  def aggregateAge(array)
+    result = [["20代",0],["30代",0],["40代",0],["50代",0],["60代",0],["その他",0]]
+    array.each do |i|
+      if 20 <= i && i < 30
+        result[0][1] += 1
+      elsif 30 <= i && i < 40
+        result[1][1] += 1
+      elsif 40 <= i && i < 50
+        result[2][1] += 1
+      elsif 50 <= i && i < 60
+        result[3][1] += 1
+      elsif 60 <= i && i < 70
+        result[4][1] += 1
+      else
+        result[5][1] += 1
+      end
+    end
+    return result
+  end
+
+  def aggregateIncome(array)
+    result = [["300万円未満",0],["300~400万円代",0],["500~600万円代",0],["700~800万円代",0],["900~1000万円代",0],["1000万円以上",0]]
+    array.each do |i|
+      if i < 300
+        result[0][1] += 1
+      elsif 300 <= i && i < 500
+        result[1][1] += 1
+      elsif 500 <= i && i < 700
+        result[2][1] += 1
+      elsif 700 <= i && i < 900
+        result[3][1] += 1
+      elsif 900 <= i && i < 2000
+        result[4][1] += 1
+      else
+        result[5][1] += 1
+      end
+    end
+    return result
+  end
+
+  def aggregateFamilyForm(array)
+    result = [["夫婦",0],["単身",0],["ファミリー",0]]
+    array.each do |i|
+      if i == "couple"
+        result[0][1] += 1
+      elsif i == "single"
+        result[1][1] += 1
+      else
+        result[2][1] += 1
+      end
+    end
+    return result
+  end
+
+  def aggregateJobPeriod(array)
+    result = [["1年未満",0],["1~2年",0],["3年以上",0]]
+    array.each do |i|
+      if i == "less1year"
+        result[0][1] += 1
+      elsif i == "to2year"
+        result[1][1] += 1
+      else
+        result[2][1] += 1
+      end
+    end
+    return result
   end
 
   def favorites
---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,5 @@
+Chartkick.options = {
+	messages: {empty: "No data"},
+	colors: ["#0051e0", "#ffc107", "#4CC709", "#EE1401", "#F91A85", "#A31AF9"]
+}
+
